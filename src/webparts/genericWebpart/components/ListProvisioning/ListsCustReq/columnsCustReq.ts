bb90e8a8d8ec258f7aa6d9a99b0606200ffb5ab5
Karina Stan <karina.stan@autoliv.com>
2020-08-22 00:13:30 +0300

    Did columns and views for CstReq

--- src/webparts/genericWebpart/components/ListProvisioning/ListsCustReq/columnsCustReq.ts
+++ src/webparts/genericWebpart/components/ListProvisioning/ListsCustReq/columnsCustReq.ts
@@ -1,1 +1,276 @@
 
+//  >>>> ADD import additional controls/components
+import { UrlFieldFormatType, Field } from "@pnp/sp/presets/all";
+import { IFieldAddResult, FieldTypes, IFieldInfo, IField,
+    ChoiceFieldFormatType,
+    DateTimeFieldFormatType, CalendarType, DateTimeFieldFriendlyFormatType,
+    FieldUserSelectionMode, IFieldCreationProperties } from "@pnp/sp/fields/types";
+
+import { IMyFieldTypes, IBaseField , ITextField , IMultiLineTextField , INumberField , IXMLField ,
+    IBooleanField , ICalculatedField , IDateTimeField , ICurrencyField , IUserField , ILookupField , IChoiceField ,
+    IMultiChoiceField , IDepLookupField , ILocationField, IURLField } from '../../../../../services/listServices/columnTypes';
+
+import { cBool, cCalcN, cCalcT, cChoice, cMChoice, cCurr, cDate, cLocal, cLook, cDLook,
+    cMText, cText, cNumb, cURL, cUser, cMUser, MyFieldDef, minInfinity, maxInfinity,  cSLook, cComputed,  } from '../../../../../services/listServices/columnTypes';
+
+//import { statusChoices, defStatus }  from '../../webparts/genericWebpart/components/GenericWebpart';
+
+//Imported but not used so that intellisense can prevent duplicate named columns.
+import { ootbID, ootbTitle, ootbEditor, ootbAuthor, ootbCreated, ootbModified, } from '../../../../../services/listServices/columnsOOTB';
+
+/***
+ *     .d8b.  d8888b. d8888b.       d888b  d8888b.  .d88b.  db    db d8888b.      d8b   db  .d8b.  .88b  d88. d88888b
+ *    d8' `8b 88  `8D 88  `8D      88' Y8b 88  `8D .8P  Y8. 88    88 88  `8D      888o  88 d8' `8b 88'YbdP`88 88'
+ *    88ooo88 88   88 88   88      88      88oobY' 88    88 88    88 88oodD'      88V8o 88 88ooo88 88  88  88 88ooooo
+ *    88~~~88 88   88 88   88      88  ooo 88`8b   88    88 88    88 88~~~        88 V8o88 88~~~88 88  88  88 88~~~~~
+ *    88   88 88  .8D 88  .8D      88. ~8~ 88 `88. `8b  d8' 88b  d88 88           88  V888 88   88 88  88  88 88.
+ *    YP   YP Y8888D' Y8888D'       Y888P  88   YD  `Y88P'  ~Y8888P' 88           VP   V8P YP   YP YP  YP  YP Y88888P
+ *
+ *
+ */
+
+const thisColumnGroup = 'Customer Requirements';
+const thisDefaultDescription = 'Customer Requirements library';
+
+
+
+/***
+ *    d88888b db    db  .d8b.  .88b  d88. d8888b. db      d88888b       .o88b.  .d88b.  db      db    db .88b  d88. d8b   db .d8888.
+ *    88'     `8b  d8' d8' `8b 88'YbdP`88 88  `8D 88      88'          d8P  Y8 .8P  Y8. 88      88    88 88'YbdP`88 888o  88 88'  YP
+ *    88ooooo  `8bd8'  88ooo88 88  88  88 88oodD' 88      88ooooo      8P      88    88 88      88    88 88  88  88 88V8o 88 `8bo.
+ *    88~~~~~  .dPYb.  88~~~88 88  88  88 88~~~   88      88~~~~~      8b      88    88 88      88    88 88  88  88 88 V8o88   `Y8b.
+ *    88.     .8P  Y8. 88   88 88  88  88 88      88booo. 88.          Y8b  d8 `8b  d8' 88booo. 88b  d88 88  88  88 88  V888 db   8D
+ *    Y88888P YP    YP YP   YP YP  YP  YP 88      Y88888P Y88888P       `Y88P'  `Y88P'  Y88888P ~Y8888P' YP  YP  YP VP   V8P `8888Y'
+ *
+ *
+ */
+
+export const example : ITextField = {
+    fieldType: cText,
+    name: 'xyz',
+    title: 'xyz Title visible',
+    maxLength: 255,
+    onCreateProps: {
+        Group: thisColumnGroup,
+        Description: thisDefaultDescription,
+    },
+    onCreateChanges: {
+        //Hidden: true,
+        Title: 'xyz Title Updated on Create',
+    },
+    showNew: true,
+    showEdit: true,
+    showDisplay: false,
+    changes1: { Title: 'xyz Title changes1' },  //Properties you want changed any time in your code
+    changes2: { Title: 'xyz Title changes2' },  //Properties you want changed any time in your code
+    changes3: { Title: 'xyz Title changes3' },  //Properties you want changed any time in your code
+    changesFinal: { Title: 'xyz Title changesFinal' },  //Properties you want changed at the very end... like hiding fields once formula columns are created and views are also created (can't add to view if it's hidden)
+
+    //showDisplay: false,
+};
+
+/***
+ *    d8888b. d88888b  .d8b.  db            .o88b.  .d88b.  db      db    db .88b  d88. d8b   db .d8888.
+ *    88  `8D 88'     d8' `8b 88           d8P  Y8 .8P  Y8. 88      88    88 88'YbdP`88 888o  88 88'  YP
+ *    88oobY' 88ooooo 88ooo88 88           8P      88    88 88      88    88 88  88  88 88V8o 88 `8bo.
+ *    88`8b   88~~~~~ 88~~~88 88           8b      88    88 88      88    88 88  88  88 88 V8o88   `Y8b.
+ *    88 `88. 88.     88   88 88booo.      Y8b  d8 `8b  d8' 88booo. 88b  d88 88  88  88 88  V888 db   8D
+ *    88   YD Y88888P YP   YP Y88888P       `Y88P'  `Y88P'  Y88888P ~Y8888P' YP  YP  YP VP   V8P `8888Y'
+ *
+ *
+ */
+
+
+
+let theseDSubjectChoices = ["Appendix", "Award Letter", "BOM", "Correspondence", "Cost Breakdown", "Drawings, Design", "Letter of Intent", "Organization", "Other Cust Doc", "Packaging, Freight", "Quality", "RASIC", "RFQ", "SOR", "SOW", "Specification", "Tech Review", "Terms & Conditions", "Timing", "Tooling Breakdown", "Validation", "Volumes, Rates", "Other"];
+export const DocSubjectCReq : IChoiceField = {
+    fieldType: cChoice,
+    name: 'DocSubject',
+    choices: theseDSubjectChoices,
+    onCreateProps: {
+        Group: thisColumnGroup,
+        Description: thisDefaultDescription,
+        DefaultFormula:'="' + theseDSubjectChoices[theseDSubjectChoices.length-1] + '"', // put the name you chose in the first line of the choice column, the one starting with let
+        Indexed: true,
+    },
+//    onCreateChanges: {
+//        Title: 'Status',
+//    }
+};
+
+export const zzzFileStatusCReq : IChoiceField = {
+    fieldType: cChoice,
+    name: 'zzzFileStatus',
+    choices: ["Yes", "No"],
+    onCreateProps: {
+        Group: thisColumnGroup,
+        Description: thisDefaultDescription,
+        DefaultFormula:'="Yes"' ,
+        Indexed: true,
+    },
+//    onCreateChanges: {
+//        Title: 'Status',
+//    }
+};
+
+export const IssueDateCReq : IDateTimeField = {
+    fieldType: cDate,
+    name: 'IssueDate',
+    onCreateProps: {
+        Group: thisColumnGroup,
+        Description: thisDefaultDescription,
+    },
+//    onCreateChanges: {
+//        Title: 'Due Date',
+//    }
+};
+
+let theseQuotePhaseChoices = ["1. Pre-Quote", "2. Quote", "3. Sourcing", "4. Launch", "5. Maintainace", "6. Other"];
+export const QuotePhaseCReq : IChoiceField = {
+    fieldType: cChoice,
+    name: 'QuotePhase',
+    choices: theseQuotePhaseChoices,
+    onCreateProps: {
+        Group: thisColumnGroup,
+        Description: thisDefaultDescription,
+        DefaultFormula:'="' + theseDSubjectChoices[theseDSubjectChoices.length-1] + '"',
+        Indexed: true,
+    },
+//    onCreateChanges: {
+//        Title: 'Status',
+//    }
+};
+
+export const RequirementNoCReq : ITextField = {
+    fieldType: cText,
+    name: 'RequirementNo',
+    maxLength: 255,
+    onCreateProps: {
+        Group: thisColumnGroup,
+        Description: thisDefaultDescription,
+    }
+};
+
+
+export const MYCReq : ITextField = {
+    fieldType: cText,
+    name: 'MY',
+    maxLength: 10,
+    onCreateProps: {
+        Group: thisColumnGroup,
+        Description: thisDefaultDescription,
+    }
+};
+
+export const ProductItemCReq : ITextField = {
+    fieldType: cText,
+    name: 'ProductItem',
+    maxLength: 10,
+    onCreateProps: {
+        Group: thisColumnGroup,
+        Description: thisDefaultDescription,
+    }
+};
+
+export const ProgramCReq : ITextField = {
+    fieldType: cText,
+    name: 'Program',
+    maxLength: 20,
+    onCreateProps: {
+        Group: thisColumnGroup,
+        Description: thisDefaultDescription,
+    }
+};
+
+
+export const DateRequirementPhaseCReq : ICalculatedField = {
+    fieldType: cCalcT,
+    name: 'DateRequirementPhase',
+    formula: '=TEXT([IssueDate],"YYYY-MM-DD")&": "&RequirementNo&" ("&QuotePhase&")"',
+    onCreateProps: {
+        Group: thisColumnGroup,
+        Description: thisDefaultDescription,
+    },
+};
+
+
+export const PhaseDateRequirementCReq : ICalculatedField = {
+    fieldType: cCalcT,
+    name: 'PhaseDateRequirement',
+    formula: '=QuotePhase&": "&TEXT([IssueDate],"YYYY-MM-DD")&" - "&RequirementNo',
+    onCreateProps: {
+        Group: thisColumnGroup,
+        Description: thisDefaultDescription,
+    },
+};
+
+
+export const RequirementDatePhaseCReq : ICalculatedField = {
+    fieldType: cCalcT,
+    name: 'RequirementDatePhase',
+    formula: '=RequirementNo&": "&TEXT([IssueDate],"YYYY-MM-DD")&" "&" ("&QuotePhase&")"',
+    onCreateProps: {
+        Group: thisColumnGroup,
+        Description: thisDefaultDescription,
+    },
+};
+
+
+/***
+ *    d88888b db    db d8888b.  .d88b.  d8888b. d888888b
+ *    88'     `8b  d8' 88  `8D .8P  Y8. 88  `8D `~~88~~'
+ *    88ooooo  `8bd8'  88oodD' 88    88 88oobY'    88
+ *    88~~~~~  .dPYb.  88~~~   88    88 88`8b      88
+ *    88.     .8P  Y8. 88      `8b  d8' 88 `88.    88
+ *    Y88888P YP    YP 88       `Y88P'  88   YD    YP
+ *
+ *
+ */
+/***
+ *     .o88b.  .d88b.  db      db    db .88b  d88. d8b   db       .d8b.  d8888b. d8888b.  .d8b.  db    db .d8888.
+ *    d8P  Y8 .8P  Y8. 88      88    88 88'YbdP`88 888o  88      d8' `8b 88  `8D 88  `8D d8' `8b `8b  d8' 88'  YP
+ *    8P      88    88 88      88    88 88  88  88 88V8o 88      88ooo88 88oobY' 88oobY' 88ooo88  `8bd8'  `8bo.
+ *    8b      88    88 88      88    88 88  88  88 88 V8o88      88~~~88 88`8b   88`8b   88~~~88    88      `Y8b.
+ *    Y8b  d8 `8b  d8' 88booo. 88b  d88 88  88  88 88  V888      88   88 88 `88. 88 `88. 88   88    88    db   8D
+ *     `Y88P'  `Y88P'  Y88888P ~Y8888P' YP  YP  YP VP   V8P      YP   YP 88   YD 88   YD YP   YP    YP    `8888Y'
+ *
+ *
+ */
+
+/**
+ * This just creates an array of fields for the build/test sequence
+ * Each list would have an array of field objects like this.
+ */
+
+
+export function HarmonieEmailFields() {
+    //return null;
+
+    let theseFields: IMyFieldTypes[] = HarmonieFields('Emails');
+
+    console.log('HarmonieEmailFields', theseFields);
+    return theseFields;
+}
+
+
+export function HarmonieFields(listName: 'Emails' | 'Emails') {
+
+    let theseFields: IMyFieldTypes[] = [];
+    theseFields.push(DocSubjectCReq);  //BOTH
+    theseFields.push(zzzFileStatusCReq);  //BOTH
+    theseFields.push(IssueDateCReq);  //BOTH
+    theseFields.push(QuotePhaseCReq);  //BOTH
+    theseFields.push(RequirementNoCReq);  //BOTH
+    theseFields.push(MYCReq);  //BOTH
+    theseFields.push(ProductItemCReq);  //BOTH
+    theseFields.push(ProgramCReq);  //BOTH
+    theseFields.push(DateRequirementPhaseCReq);  //BOTH
+    theseFields.push(PhaseDateRequirementCReq);  //BOTH
+    theseFields.push(RequirementDatePhaseCReq);  //BOTH
+
+
+    return theseFields;
+
+}
+
+
--- src/webparts/genericWebpart/components/ListProvisioning/ListsCustReq/viewsCustReq.ts
+++ src/webparts/genericWebpart/components/ListProvisioning/ListsCustReq/viewsCustReq.ts
@@ -1,1 +1,122 @@
 
+//  >>>> ADD import additional controls/components
+import { UrlFieldFormatType, Field } from "@pnp/sp/presets/all";
+import { IFieldAddResult, FieldTypes, IFieldInfo, IField,
+    ChoiceFieldFormatType,
+    DateTimeFieldFormatType, CalendarType, DateTimeFieldFriendlyFormatType,
+    FieldUserSelectionMode, IFieldCreationProperties } from "@pnp/sp/fields/types";
+
+import { IMyFieldTypes, IBaseField , ITextField , IMultiLineTextField , INumberField , IXMLField ,
+    IBooleanField , ICalculatedField , IDateTimeField , ICurrencyField , IUserField , ILookupField , IChoiceField ,
+    IMultiChoiceField , IDepLookupField , ILocationField, IURLField } from '../../../../../services/listServices/columnTypes';
+
+import { MyFieldDef, } from '../../../../../services/listServices/columnTypes';
+
+import { cBool, cCalcN, cCalcT, cChoice, cMChoice, cCurr, cDate, cLocal, cLook, cDLook,
+	cMText, cText, cNumb, cURL, cUser, cMUser, minInfinity, maxInfinity } from '../../../../../services/listServices/columnTypes';
+
+import { IMyView, } from '../../../../../services/listServices/viewTypes';
+import { Eq, Ne, Lt, Gt, Leq, Geq, IsNull, IsNotNull, Contains, BeginsWith } from '../../../../../services/listServices/viewTypes';
+
+import { spliceCopyArray } from '../../../../../services/arrayServices';
+
+//Standard Queries
+import { queryValueCurrentUser, queryValueToday } from '../../../../../services/listServices/viewTypes';
+
+import { testAlertsView, createRecentUpdatesView } from '../../../../../services/listServices/viewsGeneric';
+
+/**
+ * For Importing columns, it's best to create one view file per list and only import the columns from that list :
+ */
+
+//Imported but not used so that intellisense can prevent duplicate named columns.
+import { ootbID, ootbVersion, ootbTitle, ootbEditor, ootbAuthor, ootbCreated, ootbModified, } from '../../../../../services/listServices/columnsOOTB';
+
+//Harmonie columns
+import {
+DocSubjectCReq , zzzFileStatusCReq , IssueDateCReq , QuotePhaseCReq ,
+RequirementNoCReq , MYCReq , ProductItemCReq , ProgramCReq , DateRequirementPhaseCReq ,
+PhaseDateRequirementCReq , RequirementDatePhaseCReq
+
+} from './columnsCustReq';
+//let checks = StepChecks(0,5);  // Email
+
+export const stdViewFields = [ootbID, DocSubjectCReq, ProgramCReq, ProductItemCReq, MYCReq, IssueDateCReq, QuotePhaseCReq, RequirementNoCReq, ];
+
+export const  CustRecentUpdatesFields = spliceCopyArray ( stdViewFields, null, null, 2, [ootbModified, ootbEditor ] );
+
+export const CustAllItemsView : IMyView = {
+    Title: 'All Documents', //'All Items',  --- All Documents is default view for a library
+    iFields: 	stdViewFields,
+    wheres: 	[ 	{field: ootbModified, clause:'And', 	oper: Geq, 	val: queryValueToday(-730) }, //Recently defined as last 2 years max (for indexing)
+            ],
+    orders: [ {field: ootbModified, asc: false} ],
+};
+
+
+let allFields = ["Edit", ootbTitle, DocSubjectCReq , zzzFileStatusCReq , IssueDateCReq , QuotePhaseCReq ,
+    RequirementNoCReq , MYCReq , ProductItemCReq , ProgramCReq , DateRequirementPhaseCReq ,
+    PhaseDateRequirementCReq , RequirementDatePhaseCReq];
+
+export const AllFieldsView : IMyView = {
+    Title: 'zTest - All Fields',
+    iFields: 	allFields,
+    orders: [ {field: IssueDateCReq, asc: false} ],
+
+};
+
+export const ItemsByMYView : IMyView = {
+    Title: 'Files By MY',
+    iFields: 	stdViewFields,
+    orders: [ {field: IssueDateCReq, asc: false} ],
+    groups: { collapse: true, limit: 30,
+		fields: [
+			{field: MYCReq, asc: false},
+		],
+	},
+};
+
+export const ItemsByProgramView : IMyView = {
+    Title: 'Files By Program',
+    iFields: 	stdViewFields,
+    orders: [ {field: IssueDateCReq, asc: false} ],
+    groups: { collapse: true, limit: 30,
+		fields: [
+			{field: ProgramCReq, asc: false},
+		],
+	},
+};
+
+export const ItemsByProductView : IMyView = {
+    Title: 'Files By Product',
+    iFields: 	stdViewFields,
+    orders: [ {field: IssueDateCReq, asc: false} ],
+    groups: { collapse: true, limit: 30,
+		fields: [
+			{field: ProductItemCReq, asc: false},
+		],
+	},
+};
+
+export const ItemsByDocSubjectView : IMyView = {
+    Title: 'Files By DocSubject',
+    iFields: 	stdViewFields,
+    orders: [ {field: IssueDateCReq, asc: false} ],
+    groups: { collapse: true, limit: 30,
+		fields: [
+			{field: DocSubjectCReq, asc: false},
+		],
+	},
+};
+
+export const HarmonieViews : IMyView[] = [
+    CustAllItemsView, createRecentUpdatesView( CustRecentUpdatesFields),
+    ItemsByMYView,
+    ItemsByProgramView,
+    ItemsByProductView,
+    ItemsByDocSubjectView,
+
+
+] ;
+
+
