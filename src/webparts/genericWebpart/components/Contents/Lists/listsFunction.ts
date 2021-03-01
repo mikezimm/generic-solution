75c6379dbd91ab54b39622147064ca6263ce2615
mikezimm <49648086+mikezimm@users.noreply.github.com>
2021-03-01 17:56:43 -0500

    Move some functions to npmFunctions and add ascii art to those affected files

--- src/webparts/genericWebpart/components/Contents/Lists/listsFunction.ts
+++ src/webparts/genericWebpart/components/Contents/Lists/listsFunction.ts
@@ -1,3 +1,15 @@
+
+/***
+ *    d888888b .88b  d88. d8888b.  .d88b.  d8888b. d888888b       .d88b.  d88888b d88888b d888888b  .o88b. d888888b  .d8b.  db      
+ *      `88'   88'YbdP`88 88  `8D .8P  Y8. 88  `8D `~~88~~'      .8P  Y8. 88'     88'       `88'   d8P  Y8   `88'   d8' `8b 88      
+ *       88    88  88  88 88oodD' 88    88 88oobY'    88         88    88 88ooo   88ooo      88    8P         88    88ooo88 88      
+ *       88    88  88  88 88~~~   88    88 88`8b      88         88    88 88~~~   88~~~      88    8b         88    88~~~88 88      
+ *      .88.   88  88  88 88      `8b  d8' 88 `88.    88         `8b  d8' 88      88        .88.   Y8b  d8   .88.   88   88 88booo. 
+ *    Y888888P YP  YP  YP 88       `Y88P'  88   YD    YP          `Y88P'  YP      YP      Y888888P  `Y88P' Y888888P YP   YP Y88888P 
+ *                                                                                                                                  
+ *                                                                                                                                  
+ */
+
 import { Web, IList } from "@pnp/sp/presets/all";
 
 import { sp } from "@pnp/sp";
@@ -6,6 +18,18 @@ import "@pnp/sp/webs";
 import "@pnp/sp/clientside-pages/web";
 import { ClientsideWebpart } from "@pnp/sp/clientside-pages";
 import { CreateClientsidePage, PromotedState, ClientsidePageLayoutType, ClientsideText,  } from "@pnp/sp/clientside-pages";
+import { mergeAriaAttributeValues } from "office-ui-fabric-react";
+
+/***
+ *    d888888b .88b  d88. d8888b.  .d88b.  d8888b. d888888b      d8b   db d8888b. .88b  d88.      d88888b db    db d8b   db  .o88b. d888888b d888888b  .d88b.  d8b   db .d8888. 
+ *      `88'   88'YbdP`88 88  `8D .8P  Y8. 88  `8D `~~88~~'      888o  88 88  `8D 88'YbdP`88      88'     88    88 888o  88 d8P  Y8 `~~88~~'   `88'   .8P  Y8. 888o  88 88'  YP 
+ *       88    88  88  88 88oodD' 88    88 88oobY'    88         88V8o 88 88oodD' 88  88  88      88ooo   88    88 88V8o 88 8P         88       88    88    88 88V8o 88 `8bo.   
+ *       88    88  88  88 88~~~   88    88 88`8b      88         88 V8o88 88~~~   88  88  88      88~~~   88    88 88 V8o88 8b         88       88    88    88 88 V8o88   `Y8b. 
+ *      .88.   88  88  88 88      `8b  d8' 88 `88.    88         88  V888 88      88  88  88      88      88b  d88 88  V888 Y8b  d8    88      .88.   `8b  d8' 88  V888 db   8D 
+ *    Y888888P YP  YP  YP 88       `Y88P'  88   YD    YP         VP   V8P 88      YP  YP  YP      YP      ~Y8888P' VP   V8P  `Y88P'    YP    Y888888P  `Y88P'  VP   V8P `8888Y' 
+ *                                                                                                                                                                              
+ *                                                                                                                                                                              
+ */
 
 import { IContentsListInfo, IMyListInfo, IServiceLog, IContentsLists,  } from '@mikezimm/npmfunctions/dist/listTypes'; //Import view arrays for Time list
 
@@ -13,115 +37,77 @@ import { changes, IMyFieldTypes } from '@mikezimm/npmfunctions/dist/columnTypes'
 
 import { IMyView,  } from '@mikezimm/npmfunctions/dist/viewTypes'; //Import view arrays for Time list
 
-import { addTheseItemsToList, addTheseItemsToListInBatch } from '../../../../../services/listServices/listServices';
-
 import { makeSmallTimeObject, ITheTime} from '@mikezimm/npmfunctions/dist/dateServices';
 
 import { doesObjectExistInArray, addItemToArrayIfItDoesNotExist } from '@mikezimm/npmfunctions/dist/arrayServices';
 
+import { SystemLists, TempSysLists, TempContLists, entityMaps, EntityMapsNames } from '@mikezimm/npmfunctions/dist/Constants';
+
 import { encodeDecodeString } from '@mikezimm/npmfunctions/dist/stringServices';
 
 import { getHelpfullError, } from '@mikezimm/npmfunctions/dist/ErrorHandler';
+
+/***
+ *    d888888b .88b  d88. d8888b.  .d88b.  d8888b. d888888b      .d8888. d88888b d8888b. db    db d888888b  .o88b. d88888b .d8888. 
+ *      `88'   88'YbdP`88 88  `8D .8P  Y8. 88  `8D `~~88~~'      88'  YP 88'     88  `8D 88    88   `88'   d8P  Y8 88'     88'  YP 
+ *       88    88  88  88 88oodD' 88    88 88oobY'    88         `8bo.   88ooooo 88oobY' Y8    8P    88    8P      88ooooo `8bo.   
+ *       88    88  88  88 88~~~   88    88 88`8b      88           `Y8b. 88~~~~~ 88`8b   `8b  d8'    88    8b      88~~~~~   `Y8b. 
+ *      .88.   88  88  88 88      `8b  d8' 88 `88.    88         db   8D 88.     88 `88.  `8bd8'    .88.   Y8b  d8 88.     db   8D 
+ *    Y888888P YP  YP  YP 88       `Y88P'  88   YD    YP         `8888Y' Y88888P 88   YD    YP    Y888888P  `Y88P' Y88888P `8888Y' 
+ *                                                                                                                                 
+ *                                                                                                                                 
+ */
+
+import { addTheseItemsToList, addTheseItemsToListInBatch } from '../../../../../services/listServices/listServices';
+
 
 import { IFieldLog, addTheseFields } from '../../../../../services/listServices/columnServices'; //Import view arrays for Time list
 
 import { IViewLog, addTheseViews } from '../../../../../services/listServices/viewServices'; //Import view arrays for Time list
 
 import { IAnyArray } from  '../../../../../services/listServices/listServices';
-import { mergeAriaAttributeValues } from "office-ui-fabric-react";
+
+
+ /***
+ *    d888888b .88b  d88. d8888b.  .d88b.  d8888b. d888888b      db   db d88888b db      d8888b. d88888b d8888b. .d8888. 
+ *      `88'   88'YbdP`88 88  `8D .8P  Y8. 88  `8D `~~88~~'      88   88 88'     88      88  `8D 88'     88  `8D 88'  YP 
+ *       88    88  88  88 88oodD' 88    88 88oobY'    88         88ooo88 88ooooo 88      88oodD' 88ooooo 88oobY' `8bo.   
+ *       88    88  88  88 88~~~   88    88 88`8b      88         88~~~88 88~~~~~ 88      88~~~   88~~~~~ 88`8b     `Y8b. 
+ *      .88.   88  88  88 88      `8b  d8' 88 `88.    88         88   88 88.     88booo. 88      88.     88 `88. db   8D 
+ *    Y888888P YP  YP  YP 88       `Y88P'  88   YD    YP         YP   YP Y88888P Y88888P 88      Y88888P 88   YD `8888Y' 
+ *                                                                                                                       
+ *                                                                                                                       
+ */
+
+ /***
+ *    d888888b .88b  d88. d8888b.  .d88b.  d8888b. d888888b       .o88b.  .d88b.  .88b  d88. d8888b.  .d88b.  d8b   db d88888b d8b   db d888888b 
+ *      `88'   88'YbdP`88 88  `8D .8P  Y8. 88  `8D `~~88~~'      d8P  Y8 .8P  Y8. 88'YbdP`88 88  `8D .8P  Y8. 888o  88 88'     888o  88 `~~88~~' 
+ *       88    88  88  88 88oodD' 88    88 88oobY'    88         8P      88    88 88  88  88 88oodD' 88    88 88V8o 88 88ooooo 88V8o 88    88    
+ *       88    88  88  88 88~~~   88    88 88`8b      88         8b      88    88 88  88  88 88~~~   88    88 88 V8o88 88~~~~~ 88 V8o88    88    
+ *      .88.   88  88  88 88      `8b  d8' 88 `88.    88         Y8b  d8 `8b  d8' 88  88  88 88      `8b  d8' 88  V888 88.     88  V888    88    
+ *    Y888888P YP  YP  YP 88       `Y88P'  88   YD    YP          `Y88P'  `Y88P'  YP  YP  YP 88       `Y88P'  VP   V8P Y88888P VP   V8P    YP    
+ *                                                                                                                                               
+ *                                                                                                                                               
+ */
 
 import { pivCats, IListBucketInfo } from './listsComponent';
 
+
+/***
+ *    d88888b db    db d8888b.  .d88b.  d8888b. d888888b      d888888b d8b   db d888888b d88888b d8888b. d88888b  .d8b.   .o88b. d88888b .d8888. 
+ *    88'     `8b  d8' 88  `8D .8P  Y8. 88  `8D `~~88~~'        `88'   888o  88 `~~88~~' 88'     88  `8D 88'     d8' `8b d8P  Y8 88'     88'  YP 
+ *    88ooooo  `8bd8'  88oodD' 88    88 88oobY'    88            88    88V8o 88    88    88ooooo 88oobY' 88ooo   88ooo88 8P      88ooooo `8bo.   
+ *    88~~~~~  .dPYb.  88~~~   88    88 88`8b      88            88    88 V8o88    88    88~~~~~ 88`8b   88~~~   88~~~88 8b      88~~~~~   `Y8b. 
+ *    88.     .8P  Y8. 88      `8b  d8' 88 `88.    88           .88.   88  V888    88    88.     88 `88. 88      88   88 Y8b  d8 88.     db   8D 
+ *    Y88888P YP    YP 88       `Y88P'  88   YD    YP         Y888888P VP   V8P    YP    Y88888P 88   YD YP      YP   YP  `Y88P' Y88888P `8888Y' 
+ *                                                                                                                                               
+ *                                                                                                                                               
+ */
+
+ 
 export type IValidTemplate = 100 | 101;
 
 
-
-let SystemLists = ["WorkflowTasks", "Style Library",
-"SitePages", "SiteAssets", "ReusableContent", "Pages", "SearchConfigList", "OData__catalogs/masterpage", "OData__catalogs/design",
-"TeamSiteFooterQL1List", "TeamSiteFooterQL2List",
-"SiteCollectionImages", "SiteCollectionDocuments", "FormServerTemplates", "Reports List", "PublishingImages",
-"AEInspiredTilesItemsList", "AEInspiredTilesAssetsList", "PublishedFeedList", "Workflow TasksList", "AEGoalThermometerAssetsList", "AEMetroGridAssetsList", "AEMetroGridItemsList", "AEMetroGridPicLibList", "AESwipeGalleryAssetsList",
-"AESwipeGalleryDefaultImagesList", "Workflows", "Workflow HistoryList", "OData__catalogs/fpdatasources", "IWConvertedForms", "Access Requests", "Style Library",
-
-"OData__catalogs/appdata","OData__catalogs/appfiles","OData__catalogs/lt","OData__catalogs/solutions","OData__catalogs/theme","OData__catalogs/wp",
-
-"CCSAdvancedApprovalLogList","CCSWFHistory_Common_ErrorLogsList","CrowCanyonAppsEmailsList","CrowCanyonMailTemplatesList","Send MailsList","CrowCanyonAppsLib","NITROUserPreferencesList",
-
-"OData__catalogs/hubsite","OData__catalogs/MaintenanceLogs",
-
-];
-
-let TempSysLists = ["OurGroupsList", "OurTilesList", "TemplateHistoryList", "Template HistoryList",
-"TemplateReferenceList", "AE KPI ListList", "PnpPanelList",
-"SiteLaunchCheckListList", "EmailSettingsList", "YearView ConfigurationList", "SubscribeList","ProjectsList","TrackMyTimeList"
-];
-
-let TempContLists = ["ActionRegisterList", "AgendasList", "AutoOnBoardList", "BringOnBoardList", "BudgetDeptList", "BudgetFiles", "CalendarDocs", "CalendarList", "CustomerComplaints", "CustRequirements", "Deliverables", "DeskInstructions",
-"Documents2", "Documents3", "Documents4", "Documents5", "Emails", "EventDocs", "EventsList", "FAQsList", "FinanceDocs2", "FinanceDocs3", "FinanceDocs4", "FinanceDocs5", "Itineraries", "LaunchThisSiteChecklistList", "ManufacturingRecords",
-"Media", "OurForms", "OurOnBoardingList", "OurPNsList", "OurTasksList", "OurWiki", "PartTrackerList", "Performance", "PresentationLinksList", "Presentations", "ProcessProductionEquipment", "ProjectOverviewList", "QualityRecords",
-"QualitySysReporting", "ReportFiles", "RequestsList", "RFQDocs2", "RFQDocs3", "RFQDocs4", "RFQDocs5", "SerialDocuments", "Shared Documents", "SiteLaunchCheckListList", "SuggestionsList", "TasksList", "TimelineList", "ToolTrackerList",
-"TrainingRecords", "VehicleVolumesList", 
-"Smile1List", "Smile2List", "Smile3List", "Smile4List", "Smile5List", "Smile6List", "Smile7List", "Smile8List", "Smile9List", "Smile10List", "Smile11List", "Smile12List", 
-"Attachments00", "Attachments01", "Attachments02", "Attachments03", "Attachments04", "Attachments05", "Attachments06", "Attachments07", "Attachments08", "Attachments09",
-"Attachments10", "Attachments11", "Attachments12", "LessonsLearned", "ReadAcrossList", "YokotensList",
-"FilesYMCat","FilesYMCatU"
-];
-
-let entityMaps = [
-
-    { name: 'ReusableContent' , url: 'ReusableContent/Content Preview.aspx' },
-    { name: 'Style Library', url: 'Style Library' },
-    { name: 'MicroFeed', url: '/Lists/PublishedFeed/' },
-    { name: 'Long Running Operation Status', url: 'Long Running Operation Status' },
-    { name: 'Notification Pages', url: 'Notification Pages' },
-    { name: 'UserInfo', url: '_layouts/15/people.aspx' },
-    { name: 'Reports List', url: 'Reports List' },
-    { name: 'DeviceChannels', url: 'Device Channels' },
-    { name: 'Cache Profiles', url: 'Cache Profiles' },
-    { name: 'ProjectPolicyItemList', url: 'ProjectPolicyItemList' },
-    { name: 'Quick Deploy Items', url: 'Quick Deploy Items' },
-    { name: 'Relationships List', url: 'Relationships List' },
-    { name: 'SharePointHomeCacheListList', url: 'SharePointHomeCacheList' },
-    { name: 'PublishedLinks', url: 'PublishedLinks' },
-    { name: 'Translation Packages', url: 'Translation Packages' },
-    { name: 'Translation Status', url: 'Translation Status' },
-    { name: 'TaxonomyHiddenListList', url: 'Lists/TaxonomyHiddenList' },
-    { name: 'Variation Labels', url: 'Variation Labels' },
-    { name: 'IWConvertedForms', url: 'IWConvertedForms' },
-    { name: 'FormServerTemplates', url: 'FormServerTemplates' },
-    { name: 'Access Requests', url: 'Access Requests/pendingreq.aspx?mbypass=1' },
-
-    { name: 'ClientSideAssets', url: 'ClientSideAssets' },
-    { name: 'ComponentManifestsList', url: 'Lists/ComponentManifests' },
-    { name: 'ContentTypeSyncLogList', url: 'Lists/ContentTypeSyncLog' },
-    { name: 'SiteCollectionAppCatalogsList', url: 'SiteCollectionAppCatalogsList' },
-    { name: 'WebApiPermissionRequestsList', url: 'WebApiPermissionRequestsList' },
-    { name: 'AppRequests', url: 'AppRequests' },
-    { name: 'AgaveCatalog', url: 'AgaveCatalog' },
-    { name: 'AppCatalog', url: 'AppCatalog' },
-    { name: 'TenantWideExtensionsList', url: 'Lists/TaxonomyHiddenList' },
-    { name: '_catalogs/appdata', url: '_catalogs/appdata' },
-    { name: '_catalogs/appfiles', url: '_catalogs/appfiles' },
-    { name: '_catalogs/lt', url: '_catalogs/lt' },
-    { name: '_catalogs/MaintenanceLogs', url: '_catalogs/MaintenanceLogs' },
-    { name: '_catalogs/solutions', url: '_catalogs/solutions' },
-    { name: '_catalogs/theme', url: '_catalogs/theme' },
-    { name: '_catalogs/wp', url: '_catalogs/wp' },
-    { name: '_catalogs/hubsite', url: '_catalogs/hubsite' },
-    { name: '', url: '' },
-    { name: '', url: '' },
-    { name: 'CrowCanyonAppsLib', url: 'CrowCanyonAppsLib' },
-    { name: 'NITROUserPreferences', url: 'NITROUserPreferencesList' },
-    { name: '', url: '' },
-    { name: '', url: '' },
-    { name: '', url: '' },
-    { name: '', url: '' },
-    { name: '', url: '' },
-];
-
-const EntityMapsNames = entityMaps.map( e => {
-    return e.name;
-});
 
 //export async function provisionTestPage( makeThisPage:  IContentsListInfo, readOnly: boolean, setProgress: any, markComplete: any ): Promise<IServiceLog[]>{
 export async function allAvailableLists( webURL: string, listBuckets: IListBucketInfo[], addTheseListsToState: any, setProgress: any, markComplete: any ): Promise<IContentsListInfo[]>{
@@ -198,6 +184,8 @@ export async function allAvailableLists( webURL: string, listBuckets: IListBucke
 
 
 }
+
+
 
 function getListSort( theList: IContentsListInfo, listBuckets: IListBucketInfo[] ) {
 
